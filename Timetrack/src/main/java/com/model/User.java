00ef94cfebe391d9d44aa9f5cffef891351d9099
Midhula A <142242670+Midhula0@users.noreply.github.com>
2024-07-25 12:25:21 +0530

    User.java

--- Timetrack/src/main/java/com/model/User.java
+++ Timetrack/src/main/java/com/model/User.java
@@ -1,22 +1,29 @@
 package com.model;
 
 public class User {
+    private int empId;
     private String username;
     private String password;
-    private String email;
     private String role;
 
-    public User() {
-    }
+    // Constructors, getters, and setters
+    public User() {}
 
-    public User(String username, String password, String email, String role) {
+    public User(int empId, String username, String password, String role) {
+        this.empId = empId;
         this.username = username;
         this.password = password;
-        this.email = email;
         this.role = role;
     }
 
-    // Getters and Setters
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
     public String getUsername() {
         return username;
     }
@@ -31,14 +38,6 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getRole() {
